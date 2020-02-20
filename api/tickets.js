3691d178287df4f405187ad9425ea4464ddfe879
Jeremay <t-jeremy@hotmail.fr>
2020-02-20 15:46:35 +0100

    update api/ticket with post for new ticket

--- api/tickets.js
+++ api/tickets.js
@@ -40,4 +40,42 @@ router.get('/:id', (req, res) => {
 });
 
 
+function getNextSequence(name) {
+  return db.collection('counters').findOneAndUpdate(
+    { _id: name },
+    { $inc: { seq: 1 } }
+  )
+  .then(res => res.value.seq);
+}
+
+
+router.post('/new', async (req, res) => { // async for await and get the id
+  console.log(req.body);
+  db.collection('tickets').insertOne({
+    _id: await getNextSequence("ticketId"), // await for promise to get the id 
+    status: 1,
+    title: req.body.title,
+    type: req.body.type,
+    client: req.body.client,
+    requester: req.body.requester,
+    category: req.body.category,
+    description: req.body.description,
+    parentTicket: req.body.parentTicket,
+    weight: req.body.weight,
+    subTickets: [],
+    progress: 0,
+    created: Math.floor(Date.now() / 1000),
+    last_edited: Math.floor(Date.now() / 1000),
+    creator: "hardcode" // TODO recup token et nom de l'utilisateur
+  })
+  .then(result => {
+    if(result.insertedCount == 1){
+      res.send(result.ops[0]);
+    } else{
+      res.sendStatus(500);
+    }
+  })
+  
+});
+
 module.exports = router;
