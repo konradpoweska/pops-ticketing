f63fef8c072cddc952459b02fa3fe672f2fca55e
Konrad Poweska <konradpoweska5@gmail.com>
2023-03-05 11:35:01 +0100

    fix(db-init): correctly encode passwords

--- api/db-init/sample-data/users.js
+++ api/db-init/sample-data/users.js
@@ -1,17 +1,18 @@
-[
+const crypt = require('bcryptjs');
+
+module.exports = [
   {
     "username": "arnaud.lauperateur",
     "email": "arnaud.lauperateur@pops1920.fr",
     "name": "Arnaud LAUPERATEUR",
-    "password": "asdf123",
+    "password": crypt.hashSync("asdf123"),
     "rights": ["OPERATOR"]
   },
-
   {
     "username": "michel.ladmin",
     "email": "michel.ladmin@pops1920.fr",
     "name": "Michel LADMIN",
-    "password": "admin1234",
+    "password": crypt.hashSync("admin1234"),
     "rights": ["ADMIN"]
   }
 ]
